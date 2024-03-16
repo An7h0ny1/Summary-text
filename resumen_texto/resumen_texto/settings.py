efa7dd6fe3928503313f7206e731711b485f9a86
Anthony Parra <91570972+An7h0ny1@users.noreply.github.com>
2024-03-15 19:13:50 -0500

    Update settings.py

--- resumen_texto/resumen_texto/settings.py
+++ resumen_texto/resumen_texto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f-l_t56rfuo4*10vx=un&mnseprj7q_ww#9r+qtx$f$)dok7_@'
+SECRET_KEY = 'tu secret key aqui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
